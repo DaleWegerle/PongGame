04f728265fd9e34b471152c6fdae30ebec0dfadb
DaleWegerle <dalew@DESKTOP-3BI3J9N>
2020-08-27 13:25:44 +0200

    Adding final variables

--- PongGame/src/Pong.java
+++ PongGame/src/Pong.java
@@ -2,13 +2,24 @@ import javax.swing.JFrame;
 
 public class Pong extends JFrame{
 	
+	//setting the constant values to be final and static
+	//the using the variable name in place of the numbers
+	//so that if we need to change these values, we only
+	//have to do it here instead of searching the entire 
+	//program for all the functions that rely on these values
+	
+	
+	private final static String WINDOW_TITLE = "Pong";
+	private final static int WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 600;
+	
 	//Pong constructor provides configuration 
 	//for our window
 	public Pong() {
-		setTitle("Pong");
-		setSize(800, 600);
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);//window size in pixels
 		setResizable(false);
-		setVisible(true);
+		setVisible(true);//if false the window will disappear
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
