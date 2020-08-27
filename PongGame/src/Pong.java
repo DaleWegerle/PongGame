6493b27f7836b2d9b7d6b67f183dab4d244e062d
DaleWegerle <dalew@DESKTOP-3BI3J9N>
2020-08-27 13:21:44 +0200

    JFrame configuration

--- PongGame/src/Pong.java
+++ PongGame/src/Pong.java
@@ -1,8 +1,22 @@
+import javax.swing.JFrame;
 
-public class Pong {
+public class Pong extends JFrame{
+	
+	//Pong constructor provides configuration 
+	//for our window
+	public Pong() {
+		setTitle("Pong");
+		setSize(800, 600);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//calling the window
+		new Pong();
 
 	}
 
